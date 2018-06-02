34654e7d40a4451543ddb18cc8242b2d0e8bd9de
Leonard <leonardquintal@gmail.com>
2018-06-02 09:13:49 -0400

    Card scriptable

---
+++ BarrelRoll/Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "ScriptableObject/Card", order = 1)]
+public class Card : ScriptableObject {
+
+    public enum eCardType
+    {
+        Laser,
+        Missile,
+        Explosion
+    }
+
+    public new string name;
+    public string description;
+    public eCardType type;
+    public Material visuals;
+
+}
