48ac17d1d701327ba552a02d424d94157adff7ce
This_Gooch <michael@mgooch.com>
2018-06-03 22:00:24 -0400

    Added cards, made small changes to card script (more types), added some unscripted UI and placeholder art.

--- BarrelRoll/Assets/Scripts/Cards/Card.cs
+++ BarrelRoll/Assets/Scripts/Cards/Card.cs
@@ -8,13 +8,30 @@ public class Card : ScriptableObject {
     public enum eCardType
     {
         Laser,
-        Missile,
-        Explosion
+        Bomb,
+        Charge,
+        Spin,
+        Boost,
+        Uturn,
+        Environment,
+        Special
     }
+
+    public enum eCardStage
+    {
+        StageOne,
+        StageTwo,
+        StageThree
+    }
+
 
     public new string name;
     public string description;
+    [Tooltip("Base value for damage done and/or damage reduced.")]
+    public int powerLevel;
     public eCardType type;
+    public eCardStage powerStage;
+    public Sprite cardArt;
     public Material visuals;
 
 }
---
+++ BarrelRoll/Assets/Scripts/Ships/ShipManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ship", menuName = "ScriptableObject/Ship", order = 2)]
+public class ShipManager : ScriptableObject {
+
+    public enum eShipPosition
+    {
+        Attacking,
+        Evading
+    }
+
+    public int health;
+    public int shields;
+    public Animation idle;
+    public eShipPosition position;
+
+
+}
