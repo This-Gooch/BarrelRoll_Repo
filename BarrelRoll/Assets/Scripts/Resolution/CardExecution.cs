5d275deb7f0761ebe8f3fa81de79bf9b18dd8ec3
This_Gooch <michael@mgooch.com>
2018-06-08 01:42:04 -0400

    Added a work in progress flow chart. Some mostly empty scripts.

---
+++ BarrelRoll/Assets/Scripts/Resolution/CardExecution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardExecution : MonoBehaviour{
+
+	public void AttackValue()
+    {
+        //Collect attack value, set to hold till evade response is triggered.
+    }
+
+    public void EvadeValue()
+    {
+        //Trigger evade play, compare values. Call calculation action.
+    }
+}
