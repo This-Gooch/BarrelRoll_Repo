c6516def27cf2a4b4dde4fedda506f3cbad7cf8d
This_Gooch <michael@mgooch.com>
2018-06-11 00:16:36 -0400

    Added little more details to flowchart. Started on resolution/execution script.

--- BarrelRoll/Assets/Scripts/Resolution/CardExecution.cs
+++ BarrelRoll/Assets/Scripts/Resolution/CardExecution.cs
@@ -4,13 +4,101 @@ using UnityEngine;
 
 public class CardExecution : MonoBehaviour{
 
-	public void AttackValue()
+
+    public int storedAttkValue;
+    public int resolvedValue;
+    public Card.eCardType attackType;
+    //public Card.eCardType evadeType;
+
+
+	public void AttackTrigger(Card attkInfo)
     {
-        //Collect attack value, set to hold till evade response is triggered.
+        storedAttkValue = attkInfo.powerLevel;
+        attackType = attkInfo.type;
+        Debug.Log("storedAttkValue = " + storedAttkValue);
     }
 
-    public void EvadeValue()
+    public void EvadeTrigger(Card evadeInfo)
     {
-        //Trigger evade play, compare values. Call calculation action.
+        if(evadeInfo.type == Card.eCardType.Spin)
+        {
+            if(attackType == Card.eCardType.Laser)
+            {
+
+            }
+
+            if (attackType == Card.eCardType.Bomb)
+            {
+
+            }
+
+            if (attackType == Card.eCardType.Charge)
+            {
+
+            }
+        }
+
+        if (evadeInfo.type == Card.eCardType.Uturn)
+        {
+            if (attackType == Card.eCardType.Laser)
+            {
+
+            }
+
+            if (attackType == Card.eCardType.Bomb)
+            {
+
+            }
+
+            if (attackType == Card.eCardType.Charge)
+            {
+
+            }
+        }
+
+        if (evadeInfo.type == Card.eCardType.Boost)
+        {
+            if (attackType == Card.eCardType.Laser)
+            {
+
+            }
+
+            if (attackType == Card.eCardType.Bomb)
+            {
+
+            }
+
+            if (attackType == Card.eCardType.Charge)
+            {
+
+            }
+        }
+        //resolvedValue = storedAttkValue - evdValue;
+        /*
+        if(resolvedValue <= 0)
+        {
+            Debug.Log("Attack evaded");
+        }
+        else
+        {
+            Debug.Log("Evade takes " + resolvedValue + " damage.");
+        }
+        */
     }
+
+    void FullEvade()
+    {
+
+    }
+
+    void PartialEvade()
+    {
+
+    }
+
+    void NoEvade()
+    {
+
+    }
+
 }
