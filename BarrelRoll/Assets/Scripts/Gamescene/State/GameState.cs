3da652710bdff7f0e51b1c4393dad71440c171a9
Leonard <leonardquintal@gmail.com>
2018-06-30 12:51:57 -0400

    Getting started on the statemanchine

---
+++ BarrelRoll/Assets/Scripts/Gamescene/State/GameState.cs
@@ -0,0 +1,78 @@
+﻿//////////////////////////////////////////
+//	Create by Leonard Marineau-Quintal  //
+//		www.leoquintgames.com			//
+//////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    ////////////////////////////////
+    ///			Constants		 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Statics			 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///	  Serialized In Editor	 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Public			 ///
+    ////////////////////////////////
+    public enum eGameState
+    {
+        Idle,
+        Attack,
+        TransitionToDefence,
+        Defence,
+        TransitionToAttack,
+        GameOver
+    }
+
+    public eGameState m_State;
+    ////////////////////////////////
+    ///			Protected		 ///
+    ////////////////////////////////
+
+    ////////////////////////////////
+    ///			Private			 ///
+    ////////////////////////////////
+
+    #region Unity API
+    #endregion
+
+    #region Public API
+    public virtual void OnBegin()
+    {
+
+    }
+
+    public void UpdateState()
+    {
+        if (IsDone())
+        {
+            OnEnd();
+        }
+    }
+
+    public virtual bool IsDone()
+    {
+        return false;
+    }
+
+    public virtual void OnEnd()
+    {
+
+    }
+    #endregion
+
+    #region Protect
+    #endregion
+
+    #region Private
+    #endregion
+}
